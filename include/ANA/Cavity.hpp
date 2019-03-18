fc9ef4d3c75ab0033b46f7bc907792221450ad96
pgbarletta <pbarletta@gmail.com>
2019-03-18 12:48:56 -0300

    Deleted CCavity. Won't replace CGAL types for now.

--- include/ANA/Cavity.hpp
+++ include/ANA/Cavity.hpp
@@ -8,34 +8,32 @@
 
 namespace ANA {
 
-class CCavity {
+class Cavity {
 public:
-    CCavity() = default;
+    Cavity() = default;
 
-    CCavity(Delaunay T) noexcept : _triangulation(T) {}
+    Cavity(Molecule const &molecule, CellFilteringOptions const cell_opts);
 
-    CCavity(Delaunay &&T) noexcept : _triangulation(T) {}
+    void add_inner_cell(Finite_cells_iterator const &cell);
 
-    CCavity(Molecule const &molecule, CellFilteringOptions const cell_opts);
-
-    void cadd_border_tetra(CPoint const &p0, CPoint const &ip1,
+    void add_border_tetra(CPoint const &p0, CPoint const &ip1,
         CPoint const &ip2, CPoint const &ip3, double const vdw0);
 
-    void cadd_border_penta(CPoint const &p0, CPoint const &p1,
-        CPoint const &ip2, CPoint const &ip3, CPoint const &ip4,
-        CPoint const &ip5, double const vdw0, double const vdw1);
+    void add_border_penta(CPoint const &p0, CPoint const &p1, CPoint const &ip2,
+        CPoint const &ip3, CPoint const &ip4, CPoint const &ip5,
+        double const vdw0, double const vdw1);
 
-    void cadd_border_penta(CPoint const &p0, CPoint const &p1, CPoint const &p2,
+    void add_border_penta(CPoint const &p0, CPoint const &p1, CPoint const &p2,
         CPoint const &ip3, CPoint const &ip4, CPoint const &ip5,
         double const vdw0, double const vdw1, double const vdw2);
 
-    friend void write_PDB(CCavity const &hueco, std::string const &filename);
+    friend void write_PDB(Cavity const &hueco, std::string const &filename);
 
-    Delaunay _triangulation;
-    std::vector<Finite_cells_iterator> _all_cells, _included_cells;
+    std::vector<Finite_cells_iterator> _all_cells, _inner_cells, _outer_cells;
     double _volume = 0, _outer_volume = 0;
 
 private:
+    Delaunay _triangulation;
     // Border polyhedrons from the cells that intersected the convex hull.
     std::vector<CCTetrahedron> _tetra_border;
     std::vector<CTriangularPrism> _penta_border;
@@ -43,42 +41,8 @@ private:
     std::vector<int> _poly_vtx_cnt;
 };
 
-// class Cavity {
-// public:
-//     Cavity() = default;
-
-//     Cavity(Delaunay T) noexcept : _triangulation(T) {}
-
-//     Cavity(Delaunay &&T) noexcept : _triangulation(T) {}
-
-//     Cavity(Molecule const &molecule, CellFilteringOptions const cell_opts);
-
-//     void add_border_tetra(Point const &p0, Point const &ip1, Point const
-//     &ip2,
-//         Point const &ip3, double const vdw0);
-
-//     void add_border_penta(Point const &p0, Point const &p1, Point const &ip2,
-//         Point const &ip3, Point const &ip4, Point const &ip5, double const
-//         vdw0, double const vdw1);
-
-//     void add_border_penta(Point const &p0, Point const &p1, Point const &p2,
-//         Point const &ip3, Point const &ip4, Point const &ip5, double const
-//         vdw0, double const vdw1, double const vdw2);
-
-//     friend void draw_lines(Cavity const &hueco, std::string const &filename);
-
-//     Delaunay _triangulation;
-//     std::vector<Tetrahedron> _all_cells, _included_cells;
-//     VtxInfo _all_info, _included_info;
-//     double _volume = 0, _outer_volume = 0;
-
-// private:
-//     // Border polyhedrons from the cells that intersected the convex hull.
-//     std::vector<Tetrahedron> _tetra_border;
-//     std::vector<TriangularPrism> _penta_border;
-//     // Number of polyhedron vertices. Needed for output.
-//     std::vector<int> _poly_vtx_cnt;
-// };
+// Get the volume filled by the cells 4 atoms.
+double occupied_cell_vol(Finite_cells_iterator const cell_iterator);
 
 // Tool for parsing a double from input file stringstream
 double parse_double(std::stringstream &in_stream);
--- src/Cavity.cpp
+++ src/Cavity.cpp
@@ -2,8 +2,7 @@
 
 namespace ANA {
 
-CCavity::CCavity(
-    Molecule const &molecule, CellFilteringOptions const cell_opts) {
+Cavity::Cavity(Molecule const &molecule, CellFilteringOptions const cell_opts) {
 
     _triangulation.insert(molecule._data.begin(), molecule._data.end());
 
@@ -14,9 +13,18 @@ CCavity::CCavity(
             _all_cells.push_back(fc_ite);
         }
     }
+
+    return;
 }
 
-void CCavity::cadd_border_tetra(CPoint const &p0, CPoint const &ip1,
+void Cavity::add_inner_cell(Finite_cells_iterator const &cell) {
+
+    _volume += volume(cell) - occupied_cell_vol(cell);
+    _inner_cells.push_back(cell);
+    return;
+}
+
+void Cavity::add_border_tetra(CPoint const &p0, CPoint const &ip1,
     CPoint const &ip2, CPoint const &ip3, double const vdw0) {
 
     double const vol =
@@ -27,7 +35,7 @@ void CCavity::cadd_border_tetra(CPoint const &p0, CPoint const &ip1,
     return;
 }
 
-void CCavity::cadd_border_penta(CPoint const &p0, CPoint const &p1,
+void Cavity::add_border_penta(CPoint const &p0, CPoint const &p1,
     CPoint const &ip2, CPoint const &ip3, CPoint const &ip4, CPoint const &ip5,
     double const vdw0, double const vdw1) {
 
@@ -47,7 +55,7 @@ void CCavity::cadd_border_penta(CPoint const &p0, CPoint const &p1,
     return;
 }
 
-void CCavity::cadd_border_penta(CPoint const &p0, CPoint const &p1,
+void Cavity::add_border_penta(CPoint const &p0, CPoint const &p1,
     CPoint const &p2, CPoint const &ip3, CPoint const &ip4, CPoint const &ip5,
     double const vdw0, double const vdw1, double const vdw2) {
 
@@ -70,79 +78,25 @@ void CCavity::cadd_border_penta(CPoint const &p0, CPoint const &p1,
     return;
 }
 
-// Cavity::Cavity(Molecule const &molecule, CellFilteringOptions const
-// cell_opts) {
+// Get the volume filled by the cells 4 atoms.
+double occupied_cell_vol(Finite_cells_iterator const cell_iterator) {
 
-//     _triangulation.insert(molecule._data.begin(), molecule._data.end());
+    double const rdW_0 = double(cell_iterator->vertex(0)->info()._radius);
+    double const rdW_1 = double(cell_iterator->vertex(1)->info()._radius);
+    double const rdW_2 = double(cell_iterator->vertex(2)->info()._radius);
+    double const rdW_3 = double(cell_iterator->vertex(3)->info()._radius);
+    CPoint const p_0 = cell_iterator->vertex(0)->point();
+    CPoint const p_1 = cell_iterator->vertex(1)->point();
+    CPoint const p_2 = cell_iterator->vertex(2)->point();
+    CPoint const p_3 = cell_iterator->vertex(3)->point();
 
-//     Finite_cells_iterator fc_ite_end = _triangulation.finite_cells_end();
-//     for (auto fc_ite = _triangulation.finite_cells_begin();
-//          fc_ite != fc_ite_end; ++fc_ite) {
-//         if (volume(fc_ite) > cell_opts._min_CV) {
-//             _all_cells.emplace_back(fc_ite);
+    double const vtx_0_vol = csphere_sector_vol(p_0, p_1, p_2, p_3, rdW_0);
+    double const vtx_1_vol = csphere_sector_vol(p_3, p_0, p_1, p_2, rdW_3);
+    double const vtx_2_vol = csphere_sector_vol(p_2, p_3, p_0, p_1, rdW_2);
+    double const vtx_3_vol = csphere_sector_vol(p_1, p_2, p_3, p_0, rdW_1);
 
-//         }
-//     }
-// }
-
-// void Cavity::add_border_tetra(Point const &p0, Point const &ip1,
-//     Point const &ip2, Point const &ip3, double const vdw0) {
-
-//     double const vol =
-//         volume(p0, ip1, ip2, ip3) - sphere_sector_vol(p0, ip1, ip2, ip3,
-//         vdw0);
-//     _outer_volume += vol;
-
-//     _tetra_border.emplace_back(p0, ip1, ip2, ip3);
-//     return;
-// }
-
-// void Cavity::add_border_penta(Point const &p0, Point const &p1,
-//     Point const &ip2, Point const &ip3, Point const &ip4, Point const &ip5,
-//     double const vdw0, double const vdw1) {
-
-//     double const vol1 = volume(p0, p1, ip2, ip3) -
-//         sphere_sector_vol(p0, p1, ip2, ip3, vdw0) -
-//         sphere_sector_vol(p1, p0, ip2, ip3, vdw1);
-
-//     double const vol2 =
-//         volume(p1, ip2, ip3, ip4) - sphere_sector_vol(p1, ip2, ip3, ip4,
-//         vdw1);
-
-//     double const vol3 =
-//         volume(p1, ip3, ip4, ip5) - sphere_sector_vol(p1, ip3, ip4, ip5,
-//         vdw1);
-//     _outer_volume += vol1 + vol2 + vol3;
-
-//     _penta_border.emplace_back(p0, p1, ip2, ip3, ip4, ip5);
-
-//     return;
-// }
-
-// void Cavity::add_border_penta(Point const &p0, Point const &p1, Point const
-// &p2,
-//     Point const &ip3, Point const &ip4, Point const &ip5, double const vdw0,
-//     double const vdw1, double const vdw2) {
-
-//     double const vol1 = volume(p0, p1, p2, ip3) -
-//         sphere_sector_vol(p0, p1, p2, ip3, vdw0) -
-//         sphere_sector_vol(p1, p0, p2, ip3, vdw1) -
-//         sphere_sector_vol(p2, p1, p0, ip3, vdw2);
-
-//     double const vol2 = volume(p1, p2, ip3, ip4) -
-//         sphere_sector_vol(p1, p2, ip3, ip4, vdw1) -
-//         sphere_sector_vol(p2, p1, ip3, ip4, vdw2);
-
-//     double const vol3 =
-//         volume(p2, ip3, ip4, ip5) - sphere_sector_vol(p2, ip3, ip4, ip5,
-//         vdw2);
-
-//     _outer_volume += vol1 + vol2 + vol3;
-
-//     _penta_border.emplace_back(p0, p1, p2, ip3, ip4, ip5);
-
-//     return;
-// }
+    return (vtx_0_vol + vtx_1_vol + vtx_2_vol + vtx_3_vol);
+}
 
 // Tool for parsing a double from input file stringstream
 double parse_double(std::stringstream &in_stream) {
@@ -152,7 +106,7 @@ double parse_double(std::stringstream &in_stream) {
     in_stream >> temp;
     try {
         coord = std::stof(temp);
-    } catch (const std::invalid_argument &ia) {
+    } catch (std::invalid_argument const &ia) {
         // some character present
         throw std::runtime_error("Can't parse input. There may be "
                                  "non-numerical characters. Aborting.");
