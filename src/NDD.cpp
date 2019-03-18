e1ef48bbbedaf3f615ddc547fc514052163aa422
pgbarletta <pbarletta@gmail.com>
2019-03-18 12:55:38 -0300

    Finally dropping CGAL types replacements.

--- src/NDD.cpp
+++ src/NDD.cpp
@@ -11,22 +11,21 @@ int NDD_ANA(std::string const &in_filename, IncludedAreaOptions &IA_opts,
     // IA_opts._resn_proto = "1 12 21 51 68 97 111 119 71 130 133 128 16 24 84";
     // IA_opts._opt = IncludedAreaOptions::residue;
 
-    ANA::CConvexHull CH = create_cconvex_hull(protein, IA_opts);
+    ANA::ConvexHull const CH = create_convex_hull(protein, IA_opts);
 
-    ANA::CCavity hueco(protein, cell_opts);
+    ANA::Cavity hueco = Cavity(protein, cell_opts);
 
     ANA::carve_CCH_into_cavity(hueco, CH);
 
-    // ANA::NDD::ndd(hueco, CH, NDD_opts);
+    ANA::NDD::ndd(protein, hueco, CH, IA_opts, NDD_opts, in_filename);
 
     // ANA::write_output_volume(cavity_void_cells, poly_vol);
 
+    
     ANA::write_PDB(hueco, "sal.pdb");
+    std::cout << "postsal" << '\n';
     ANA::write_PDB(CH, "hull.pdb");
     printf("Volumen:  %f\n", hueco._volume + hueco._outer_volume);
-
-    ANA::NDD::Modes modo(NDD_opts._modes_ndd_filename);
-    modo.calpha_to_full_atom(in_filename);
 
     return 0;
 }
