00a921d23afda2dacd621334c0aaabf3065cdad3
pgbarletta <pbarletta@gmail.com>
2019-04-03 22:15:17 +0200

    Added Tcavity, which will become the definite cavity class.

--- include/ANA/Cavity.hpp
+++ include/ANA/Cavity.hpp
@@ -4,7 +4,7 @@
 #include <ANA/Includes.hpp>
 #include <ANA/Molecule.hpp>
 #include <ANA/Options.hpp>
-#include <ANA/Primitives.hpp>
+#include <ANA/PrimitivesUtils.hpp>
 
 namespace ANA {
 
@@ -43,6 +43,44 @@ private:
 
 // Get the volume filled by the cells 4 atoms.
 double occupied_cell_vol(Finite_cells_iterator const cell_iterator);
+
+class TCavity {
+public:
+    TCavity() = default;
+
+    TCavity(Molecule const &molecule, CellFilteringOptions const cell_opts);
+
+    void add_inner_cell(TTetrahedron const &cell, TetraInfo const &info);
+
+    void add_outer_cell(TTetrahedron const &cell, TetraInfo const &info);
+
+    void add_border_tetra(Point const &p0, Point const &ip1, Point const &ip2,
+        Point const &ip3, double const vdw0);
+
+    void add_border_penta(Point const &p0, Point const &p1, Point const &ip2,
+        Point const &ip3, Point const &ip4, Point const &ip5, double const vdw0,
+        double const vdw1);
+
+    void add_border_penta(Point const &p0, Point const &p1, Point const &p2,
+        Point const &ip3, Point const &ip4, Point const &ip5, double const vdw0,
+        double const vdw1, double const vdw2);
+
+    friend void write_PDB(TCavity const &hueco, std::string const &filename);
+
+    std::vector<TTetrahedron> _all_cells, _inner_cells, _outer_cells;
+    std::vector<TetraInfo> _all_info, _inner_info, _outer_info;
+    double _volume = 0, _outer_volume = 0;
+
+private:
+    // Border polyhedrons from the cells that intersected the convex hull.
+    std::vector<TTetrahedron> _tetra_border;
+    std::vector<TTriangularPrism> _penta_border;
+    // Number of polyhedron vertices. Needed for output.
+    std::vector<int> _poly_vtx_cnt;
+};
+
+// Get the volume filled by the cells 4 atoms.
+double occupied_cell_vol(TTetrahedron const &cell, TetraInfo const &info);
 
 // Tool for parsing a double from input file stringstream
 double parse_double(std::stringstream &in_stream);
--- src/Cavity.cpp
+++ src/Cavity.cpp
@@ -96,6 +96,115 @@ double occupied_cell_vol(Finite_cells_iterator const cell_iterator) {
 
     return (vtx_0_vol + vtx_1_vol + vtx_2_vol + vtx_3_vol);
 }
+/////////////
+// Own data structures
+/////////////
+
+TCavity::TCavity(
+    Molecule const &molecule, CellFilteringOptions const cell_opts) {
+
+    Delaunay const triangulation(molecule._data.begin(), molecule._data.end());
+
+    Finite_cells_iterator fc_ite_end = triangulation.finite_cells_end();
+    for (auto fc_ite = triangulation.finite_cells_begin(); fc_ite != fc_ite_end;
+         ++fc_ite) {
+        if (volume(fc_ite) > cell_opts._min_CV) {
+            auto const v0 = fc_ite->vertex(0);
+            auto const v1 = fc_ite->vertex(1);
+            auto const v2 = fc_ite->vertex(2);
+            auto const v3 = fc_ite->vertex(3);
+
+            _all_cells.emplace_back(
+                v0->point(), v1->point(), v2->point(), v3->point());
+            _all_info.emplace_back(
+                v0->info(), v1->info(), v2->info(), v3->info());
+        }
+    }
+
+    return;
+}
+
+void TCavity::add_inner_cell(TTetrahedron const &cell, TetraInfo const &info) {
+    _volume += volume(cell) - occupied_cell_vol(cell, info);
+    _inner_cells.push_back(cell);
+    _inner_info.push_back(info);
+    return;
+}
+
+void TCavity::add_outer_cell(TTetrahedron const &cell, TetraInfo const &info) {
+    _outer_cells.push_back(cell);
+    _inner_info.push_back(info);
+    return;
+}
+
+void TCavity::add_border_tetra(Point const &p0, Point const &ip1,
+    Point const &ip2, Point const &ip3, double const vdw0) {
+
+    double const vol =
+        volume(p0, ip1, ip2, ip3) - sphere_sector_vol(p0, ip1, ip2, ip3, vdw0);
+    _outer_volume += vol;
+
+    _tetra_border.emplace_back(p0, ip1, ip2, ip3);
+    return;
+}
+
+void TCavity::add_border_penta(Point const &p0, Point const &p1,
+    Point const &ip2, Point const &ip3, Point const &ip4, Point const &ip5,
+    double const vdw0, double const vdw1) {
+
+    double const vol1 = volume(p0, p1, ip2, ip3) -
+        sphere_sector_vol(p0, p1, ip2, ip3, vdw0) -
+        sphere_sector_vol(p1, p0, ip2, ip3, vdw1);
+
+    double const vol2 =
+        volume(p1, ip2, ip3, ip4) - sphere_sector_vol(p1, ip2, ip3, ip4, vdw1);
+
+    double const vol3 =
+        volume(p1, ip3, ip4, ip5) - sphere_sector_vol(p1, ip3, ip4, ip5, vdw1);
+    _outer_volume += vol1 + vol2 + vol3;
+
+    _penta_border.emplace_back(p0, p1, ip2, ip3, ip4, ip5);
+
+    return;
+}
+
+void TCavity::add_border_penta(Point const &p0, Point const &p1,
+    Point const &p2, Point const &ip3, Point const &ip4, Point const &ip5,
+    double const vdw0, double const vdw1, double const vdw2) {
+
+    double const vol1 = volume(p0, p1, p2, ip3) -
+        sphere_sector_vol(p0, p1, p2, ip3, vdw0) -
+        sphere_sector_vol(p1, p0, p2, ip3, vdw1) -
+        sphere_sector_vol(p2, p1, p0, ip3, vdw2);
+
+    double const vol2 = volume(p1, p2, ip3, ip4) -
+        sphere_sector_vol(p1, p2, ip3, ip4, vdw1) -
+        sphere_sector_vol(p2, p1, ip3, ip4, vdw2);
+
+    double const vol3 =
+        volume(p2, ip3, ip4, ip5) - sphere_sector_vol(p2, ip3, ip4, ip5, vdw2);
+
+    _outer_volume += vol1 + vol2 + vol3;
+
+    _penta_border.emplace_back(p0, p1, p2, ip3, ip4, ip5);
+
+    return;
+}
+
+// Get the volume filled by the cells 4 atoms.
+double occupied_cell_vol(TTetrahedron const &cell, TetraInfo const &info) {
+
+    double const vtx_0_vol =
+        sphere_sector_vol(cell[0], cell[1], cell[2], cell[3], info._radius[0]);
+    double const vtx_1_vol =
+        sphere_sector_vol(cell[3], cell[0], cell[1], cell[2], info._radius[3]);
+    double const vtx_2_vol =
+        sphere_sector_vol(cell[2], cell[3], cell[0], cell[1], info._radius[2]);
+    double const vtx_3_vol =
+        sphere_sector_vol(cell[1], cell[2], cell[3], cell[0], info._radius[1]);
+
+    return (vtx_0_vol + vtx_1_vol + vtx_2_vol + vtx_3_vol);
+}
 
 // Tool for parsing a double from input file stringstream
 double parse_double(std::stringstream &in_stream) {
