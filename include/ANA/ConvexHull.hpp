940491130187c16ba9e7d48a9cc00b1193036b52
pgbarletta <pbarletta@gmail.com>
2019-04-04 12:13:49 +0200

    New own data structure for ConvexHull, will eventually replace older one.

--- include/ANA/ConvexHull.hpp
+++ include/ANA/ConvexHull.hpp
@@ -4,7 +4,7 @@
 #include <ANA/Includes.hpp>
 #include <ANA/Molecule.hpp>
 #include <ANA/Options.hpp>
-#include <ANA/Primitives.hpp>
+#include <ANA/PrimitivesUtils.hpp>
 #include <ANA/SoAPrimitives.hpp>
 
 namespace ANA {
@@ -19,7 +19,7 @@ struct CylinderTag {};
 struct PrismTag {};
 struct FileTag {};
 
-struct ConvexHull {
+class ConvexHull {
 public:
     ConvexHull() = default;
 
@@ -81,6 +81,69 @@ public:
 };
 
 ConvexHull create_convex_hull(
+    Molecule const &protein, IncludedAreaOptions const &IA_opts);
+
+class TConvexHull {
+public:
+    TConvexHull() = default;
+
+    TConvexHull(
+        Molecule const &protein, std::string const &resn_proto, ResidueTag);
+
+    TConvexHull(
+        Molecule const &protein, std::string const &atom_proto, AtomTag);
+
+    TConvexHull(std::string const &sphere_proto, SphereTag);
+
+    TConvexHull(std::string const &cylinder_proto, CylinderTag);
+
+    TConvexHull(std::string const &prism_proto, PrismTag);
+
+    TConvexHull(std::string const &filename, FileTag);
+
+    // Run the actual Convex Hull algorithm with CGAL.
+    // Should only work with containers. Will fix w/ c++20. TODO
+    template <class T>
+    void run_convex_hull(T const &points) {
+        if (points.size() < 4) {
+            throw std::runtime_error(
+                "Not possible to triangulate less than 4 points. Aborting.");
+        }
+
+        Polyhedron CH;
+        CGAL::convex_hull_3(points.begin(), points.end(), CH);
+
+        auto const triangles = CH.size_of_facets();
+        _triangles.reserve(triangles);
+        _normals.reserve(triangles);
+
+        auto f_end = CH.facets_end();
+        for (auto f_ite = CH.facets_begin(); f_ite != f_end; ++f_ite) {
+            // Fix around the weirdest CGAL bug.
+            auto he_ite = f_ite->facet_begin();
+            Point const p0(he_ite->vertex()->point());
+            he_ite++;
+            Point const p1(he_ite->vertex()->point());
+            he_ite++;
+            Point const p2(he_ite->vertex()->point());
+            Vector const v01 = p1 - p0, v02 = p2 - p0;
+
+            _normals.emplace_back(normalize(cross_product(v01, v02)));
+            _v01.push_back(v01);
+            _v02.push_back(v02);
+            _triangles.emplace_back(p0, p1, p2);
+        }
+
+        return;
+    }
+
+    std::vector<TTriangle> _triangles;
+    std::vector<Vector> _normals, _v01, _v02;
+    // Used in case the CH was constructed from residues or atoms.
+    std::vector<int> _included_resis, _included_atoms;
+};
+
+TConvexHull tcreate_convex_hull(
     Molecule const &protein, IncludedAreaOptions const &IA_opts);
 
 } // namespace ANA
