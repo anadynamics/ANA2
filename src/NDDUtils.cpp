cfb60d184956c7243dbf1feba9771a04d173dda7
pgbarletta <pbarletta@gmail.com>
2019-04-12 12:18:19 +0200

    Done with option for writing volumes in NDD instead of the derivatives.

--- src/NDDUtils.cpp
+++ src/NDDUtils.cpp
@@ -17,33 +17,42 @@ void ndd(
 
     if (CH._dynamic) {
         for (size_t j = 0; j < modos._j; ++j) {
-
             double const mul = NDD_opts._step / modos._evals[j];
+
             // In the positive direction.
             ConvexHull CH_pos(CH, modos._evectors[j], mul);
             Cavity hueco_pos(hueco, modos._evectors[j], mul);
-
             carve_CH_into_cavity(hueco_pos, CH_pos);
             double const pos_vol = hueco_pos._volume + hueco_pos._outer_volume;
 
             // In the negative direction.
             ConvexHull CH_neg(CH, modos._evectors[j], -mul);
             Cavity hueco_neg(hueco, modos._evectors[j], -mul);
-
             carve_CH_into_cavity(hueco_neg, CH_neg);
             double const neg_vol = hueco_neg._volume + hueco_neg._outer_volume;
 
             // Numerical derivative.
-            double const der_vol =
-                ((pos_vol - neg_vol) * modos._evals[j]) / NDD_opts._step;
+            double const der_vol{(pos_vol - neg_vol) / mul};
+
             der_vols_ndd.push_back(der_vol);
             pos_vols_ndd.push_back(pos_vol);
             neg_vols_ndd.push_back(neg_vol);
         }
 
-        std::string const filename =
-            std::to_string(NDD_opts._step) + "_" + NDD_opts._out_ndd_filename;
-        write_vector(der_vols_ndd, filename);
+        if (NDD_opts._derivative) {
+            std::string const filename = std::to_string(NDD_opts._step) + "_" +
+                NDD_opts._out_ndd_filename;
+            write_vector(der_vols_ndd, filename);
+        } else {
+            std::string const filename_pos = std::to_string(NDD_opts._step) +
+                "_pos_" + NDD_opts._out_ndd_filename;
+            write_vector(pos_vols_ndd, filename_pos);
+
+            std::string const filename_neg = std::to_string(NDD_opts._step) +
+                "_neg_" + NDD_opts._out_ndd_filename;
+            write_vector(neg_vols_ndd, filename_neg);
+        }
+
     } else {
         ;
     }
