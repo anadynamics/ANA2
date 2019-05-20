cc80bc680134c55248b9fc81d17316b369c527af
pgbarletta <pbarletta@gmail.com>
2019-05-20 17:58:53 +0200

    Cosmetic changes to ProgramOptions

--- src/ProgramOptions.cpp
+++ src/ProgramOptions.cpp
@@ -22,54 +22,72 @@ int get_parameters(int ac, char *av[], ANA::InOutOptions &io_opts,
     PO::options_description CLI_only_options(help_header);
 
     CLI_only_options.add_options()
+///
+// ## Terminal options
+///
+    ("input_struct,s",
+    PO::value<std::string>(&io_opts._in_filename)->default_value("none"),
+    "Input structure. This is the only positional argument so you can type it first and skip the flag.\n")
 
-    ("input_struct,s", PO::value<std::string>(&io_opts._in_filename)
-	  ->default_value("none"), "Input structure (pdb). Positional argument.\n")
+    ("input_md,d",
+    PO::value<std::string>(&io_opts._in_md_filename)->default_value("none"),
+    "Input file with MD simulation.\n")
 
-    ("input_md,d", PO::value<std::string>(&io_opts._in_md_filename)
-    ->default_value("none"), "Input file with MD simulation.\n")
-
-    ("output_draw,f", PO::value<std::string>(&io_opts._out_pdb_filename)
-    ->default_value("none")->composing(),"PDB output filename.\n")
+    ("config_file,c",
+    PO::value<std::string>(&config_filename)->default_value("none"),
+    "Filename of the configuration file.\n")
     
-    ("out_vol,o", PO::value<std::string>(&io_opts._out_vol_filename)
-    ->default_value("none")->composing(),"Volume output filename.\n")
+    ("include,i",
+    PO::value<std::string>(&IA_opts._filename)->default_value("none")->composing(),
+    "Coordinates of the included area in PDB format.\n")
 
-    ("out_wall,w", PO::value<std::string>(&io_opts._out_wall_filename)
-    ->default_value("none")->composing(),"Cavity's wall atoms/residues output filename.\n")
-
-    ("NDD_modes,M", PO::value<std::string>(&NDD_opts._modes_ndd_filename)
-    ->default_value("none")->composing(), "Input vectors for non Delaunay dynamics (NDD).\n")
+    ("output_draw,f",
+    PO::value<std::string>(&io_opts._out_pdb_filename)->default_value("none")->composing(),
+    "PDB output filename.\n")
     
-    ("NDD_frequencies,F", PO::value<std::string>(&NDD_opts._freqs_ndd_filename)
-    ->default_value("none")->composing(), "Input frequencies to calculate the flexibility index by NDD.\n")
+    ("output_vol,o",
+    PO::value<std::string>(&io_opts._out_vol_filename)->default_value("none")->composing(),
+    "Volume output filename.\n")
 
-    ("NDD_scaling,S", PO::value<std::string>(&NDD_opts._scaling_ndd_filename)
-    ->default_value("none")->composing(), "Input scaling factors for NDD.\n")
+    ("output_wall,w",
+    PO::value<std::string>(&io_opts._out_wall_filename)->default_value("none")->composing(),
+    "Cavity's wall atoms/residues output filename.\n")
 
-    ("NDD_output,O", PO::value<std::string>(&NDD_opts._out_ndd_filename)
-    ->default_value("none")->composing(), "Suffix for the NDD output file.\n")
-
-    ("NDD_size,Z", PO::value<int>(&NDD_opts._size)
-    ->default_value(1), "Scaling number for input vectors in non Delaunay dynamics. Default: 1.\n")
-
-    ("config_file,c", PO::value<std::string>(&config_filename)
-    ->default_value("none"), "Filename of the configuration file.\n")
+    ("NDD_modes,M",
+    PO::value<std::string>(&NDD_opts._modes_ndd_filename)->default_value("none")->composing(),
+    "Input vectors for Non-Delaunay dynamics (NDD).\n")
     
-    ("include,i", PO::value<std::string>(&IA_opts._filename)
-    ->default_value("none")->composing(), "Coordinates of the included area in PDB format.\n")
+    ("NDD_frequencies,F",
+    PO::value<std::string>(&NDD_opts._freqs_ndd_filename)->default_value("none")->composing(),
+    "Input frequencies (in cm^-1) to calculate the flexibility index by NDD.\n")
 
-    ("tool_check_CH,t", PO::value<std::string>(&tool_check_CH)->default_value("none")
-    ->composing(), "Filename of output .pdb displaying the included area.\n")
+    ("NDD_scaling,S",
+    PO::value<std::string>(&NDD_opts._scaling_ndd_filename)->default_value("none")->composing(),
+    "Input scaling factors for NDD. See ANA's manual to understand why they are necessary.\n")
+
+    ("NDD_size,Z",
+    PO::value<int>(&NDD_opts._size)->default_value(1),
+    "Scaling magnitude for the input vectors for NDD. Default: 1.\n")
+
+    ("NDD_output,O",
+    PO::value<std::string>(&NDD_opts._out_ndd_filename)->default_value("none")->composing(),
+    "Suffix for the NDD output file. The scaling magnitude will be the prefix.\n")
+
+    ("tool_check_CH,t",
+    PO::value<std::string>(&tool_check_CH)->default_value("none")->composing(),
+    "Filename of output PDB displaying the included area.\n")
     
-    ("tool_pdb_to_ch,P", PO::value<std::string>(&tool_pdb_to_ch)->default_value("none")
-    ->composing(), "Read the input PDB and write 'include.ANA' file with the vertices of its convex hull.\n")
+    ("tool_pdb_to_ch,p",
+    PO::value<std::string>(&tool_pdb_to_ch)->default_value("none")->composing(),
+    "Read the input PDB and write 'include.ANA' file with the vertices of its convex hull.\n")
     
-    ("tool_pdb_norm,n", PO::value<std::string>(&tool_pdb_norm)->default_value("none")
-    ->composing(), "Read the input PDB and renumber its atoms and residues. Write the output PDB to \"tool_pdb_norm\".\n")
+    ("tool_pdb_norm,n",
+    PO::value<std::string>(&tool_pdb_norm)->default_value("none")->composing(),
+    "Read the input PDB and renumber its atoms and residues. Write the output PDB to \"tool_pdb_norm\".\n")
     
-    ("tool_aa_to_ca,a", PO::value<std::string>(&tool_aa_to_ca)->default_value("none")
-      ->composing(), "Write Calpha atoms indices for the included residues to stdout.\n")
+    ("tool_aa_to_ca,a",
+    PO::value<std::string>(&tool_aa_to_ca)->default_value("none")->composing(),
+    "Write Calpha atoms indices for the included residues to stdout.\n")
     
     ("ver,v", "Output version number.\n")
     
@@ -83,51 +101,179 @@ int get_parameters(int ac, char *av[], ANA::InOutOptions &io_opts,
     PO::options_description cfg_only_options(help_header);
     cfg_only_options.add_options()
 
-    ("included_amino_acids",
-    PO::value<std::string>(&AA_indices_proto)->default_value("none"),
-    "Amino acids that are part of a cell.\n")
+///
+// ## Configuration file options
+///
+///
+// ### Included area options
+///
+    ("included_area_residues",
+    PO::value<std::string>(&IA_opts._resn_proto)->default_value("none"),
+    "Amino acids that delimit the convex hull of the included area. "
+    "Numbers from 1 to 9999, separated by commas, spaces or tabs.\n")
 
-    ("included_area_residues", PO::value<std::string>(&IA_opts._resn_proto)
-    ->default_value("none"), "Amino acids that delimit the convex hull of the included area.\n")
+    ("included_area_atoms",
+    PO::value<std::string>(&IA_opts._atom_proto)->default_value("none"),
+    "Atoms that delimit the convex hull of the included area."
+    "Numbers from 1 to 9999, separated by commas, spaces or tabs.\n")
 
-    ("included_area_atoms", PO::value<std::string>(&IA_opts._atom_proto)
-    ->default_value("none"), "Atoms that delimit the convex hull of the included area.\n")
-
-    ("included_area_precision", PO::value<int>(&precision)
-    ->default_value(0), "0: keep all cells that intercede in the included area.\n"
-    "1: only keep null areas that are within the included area.\n"
+    ("included_area_precision",
+    PO::value<int>(&precision)->default_value(0),
+    "0: keep all cells that intersect in the included area.\n"
+    "1: prune the intersecting cells to keep only the voids that lie inside the included area.\n"
     "Default: 0.\n")
 
-    ("sphere", PO::value<std::string>(&IA_opts._sphere_proto)->default_value("none")
-    ->composing(), "Read the input coordinates and write 'include_sphere.ANA' file with the requested pseudo sphere.\n")
-    ("cylinder", PO::value<std::string>(&IA_opts._cylinder_proto)->default_value("none")
-    ->composing(), "Read the input coordinates and write 'include_cylinder.ANA' file with the requested pseudo cylinder.\n")
-    ("prism", PO::value<std::string>(&IA_opts._prism_proto)->default_value("none")
-    ->composing(), "Read the input coordinates and write 'include_prism.ANA' file with the requested prism.\n")
+    ("sphere", 
+    PO::value<std::string>(&IA_opts._sphere_proto)->default_value("none")->composing(),
+    "Read the input coordinates and write 'include_sphere.ANA' file with the requested pseudo sphere.\n")
+    ("cylinder",
+    PO::value<std::string>(&IA_opts._cylinder_proto)->default_value("none")->composing(),
+    "Read the input coordinates and write 'include_cylinder.ANA' file with the requested pseudo cylinder.\n")
+    ("prism",
+    PO::value<std::string>(&IA_opts._prism_proto)->default_value("none")->composing(),
+    "Read the input coordinates and write 'include_prism.ANA' file with the requested prism.\n")
 
-    ("triangulate_only_included_aas", PO::value<bool>(&triangulate_only_included_aas)
-    ->default_value(false), "Instead of triangulating the whole molecule triangulate only the included amino acids.\n"
-    "Default: false.\n")
+///
+// ### Useful options for outlining the desired cavity with ANA Static
+///
 
-    ("atom_only", PO::value<bool>(&atom_only)
-    ->default_value(true), "Triangulate only ATOM records.\n"
-    "Default: true.\n")
+    ("included_residues",
+    PO::value<std::string>(&AA_indices_proto)->default_value("none"),
+    "Amino acids that line the desired cavity. Useful for discovering new cavities.\n")
 
-    ("clusters_method", PO::value<std::string>(&clusters_method)->default_value("boxes"),
-    "none: don't group null areas."
-    "facets: group null areas if the tetrahedrons share facets."
-    "boxes: group null areas if the tetrahedrons bounding boxes intersect."
+    ("minimum_number_of_vertices_to_include",
+    PO::value<int>(&nbr_of_vertices_to_include)->default_value(2),
+    "Minimum number of wall atoms of the included amino acids.\n"
+    "Default: 2.\n")
+
+///
+// ### Clusters options
+///
+
+    ("clusters_method",
+    PO::value<std::string>(&clusters_method)->default_value("boxes"),
+    "Define the method to clusterize cells.\n"
+    "none: don't group null areas.\n"
+    "facets: group null areas if the tetrahedrons share facets.\n"
+    "boxes: group null areas if the tetrahedrons bounding boxes intersect.\n"
     "Default: boxes.\n")
 
-    ("clusters_min_size", PO::value<int>(&clusters_min_size)
-    ->default_value(2), "Minimum number of cells a cluster has to have to be included.\n"
+    ("clusters_min_size",
+    PO::value<int>(&clusters_min_size)->default_value(2),
+    "Minimum number of cells a cluster has to have to be included.\n"
     "Default: 2.\n")
 
-    ("minimum_number_of_vertices_to_include", PO::value<int>(
-    &nbr_of_vertices_to_include)->default_value(2), "Minimum number of wall atoms of the included amino acids.\n"
-    "Default: 2.\n")
+///
+// #### Accessible Surface Area options
+///
 
-    ("list_wall", PO::value<std::string>(&list_wall)->default_value("none"),
+    ("ASA_discard_method",
+    PO::value<std::string>(&ASA_method)->default_value("cm"),
+    "Method for discarding cells according to solvent exposure.\n"
+    " none: don't.\n"
+    "cm: determine cells surrondings using the global CM as reference.\n"
+    "backbone: draw a convex hull between the Calphas and discard every cell with its centroid outside(inside) the hull.\n"
+    "axes: determine cells surrondings using its centroid and cartesian axes as references.\n"
+    "Default: cm.\n")
+
+    ("ASA_only_side",
+    PO::value<std::string>(&only_side_ASA)->default_value("inside"),
+    "Which side to keep.\n"
+    "inside: keep inside nulls.\n"
+    "outside: keep outside nulls.\n" 
+    "Default: inside.\n")
+
+    ("ASA_exclude_residues",
+    PO::value<std::string>(&exclude_ca_for_ASA)->default_value("none"),
+    "Residues to exclude from the ASA algorithms.\n")
+
+    ("ASA_min_dot_pdt",
+    PO::value<double>(&max_probe)->default_value(0.7)->composing(),
+    "Minimum dot product to keep(discard) cell. As it increases more cells are classified as being \"outside\".\n"
+    "Default: 0.7.\n")
+
+    ("ASA_max_dist",
+    PO::value<double>(&max_probe_length)->default_value(15)->composing(),
+    "Maximum distance between cell and Calpha used to keep cell. The bigger this number is the better (but slower) the process gets.\n"
+    "Default: 15.\n")
+///
+// ### MD options
+///
+    ("start",
+    PO::value<int>(&md_start)->default_value(1),
+    "Frame to begin reading at.\n"
+    "Default: 1.\n")
+
+    ("step",
+    PO::value<int>(&md_step)->default_value(1),
+    "Step count to read trajectory frames. NetCDF format only.\n"
+    "Default: 1.\n")
+
+    ("stop",
+    PO::value<int>(&md_end)->default_value(0),
+    "Frame to stop reading. Reads the whole trajectory when set to 0.\n"
+    "Default: 0.\n")
+
+///
+// ### NDD options
+///
+    ("NDD_step",
+    PO::value<int>(&NDD_opts._step)->default_value(3),
+    "Number of steps in the NDD pipeline ANA will perform.\n"
+    "1: ANA will not perform the derivative and instead output 2 files with the volumes of the displaced cavity in the positive and the negative direction.\n"
+    "2: ANA will output the VGV.\n"
+    "3: ANA will output the flexibility index.\n"
+    "Default: 3.\n")
+
+    ("NDD_modes_format",
+    PO::value<std::string>(&NDD_opts._modes_format)->default_value("row"),
+    "Format of the input vectors.\n"
+    "amber: vectors will be read as Amber PCA modes.\n"
+    "row: vectors will be read in row major order.\n"
+    "column: vectors will be read in column major order.\n"
+    "Default: row.\n")
+
+    ("NDD_frequences_scaling",
+    PO::value<bool>(&NDD_opts._scale_w_freqs)->default_value(false),
+    "If true ANA will scale the input vectors by their frequencies instead of automatically generating its own scaling factors.\n"
+    "Default: false.\n")
+
+///
+// ### Output options
+///
+    ("output_type",
+    PO::value<std::string>(&io_opts._out_type)->default_value("grid_pdb")->composing(),
+    "How to visualize output cavities."
+    "raw_pdb: null areas as tetrahedrons.\n"
+    "raw_cgo: null areas as tetrahedrons formed with CGO lines in a pymol script.\n"
+    "grid_pdb: null areas filled with points.\n"
+    "grid_cgo: null areas filled with CGO spheres in a pymol script.\n"
+    "Default: grid_pdb.\n")
+
+    ("sphere_count",
+    PO::value<int>(&sphere_count)->default_value(5)->composing(),
+    "Sphere count for grid output.\n"
+    "Default: 5.\n")
+///
+// ### Misc options
+///
+    ("min_vol_radius",
+    PO::value<double>(&cell_opts._minVR)->default_value(1.4)->composing(),
+    "Radius of the sphere with the minimum volume to be taken into account.\n"
+    "Default: 1.4.\n")
+
+    ("max_area_radius",
+    PO::value<double>(&cell_opts._maxSR)->default_value(99)->composing(),
+    "Radius of the sphere with the maximum surface to be taken into account.\n"
+    "Default: 99.\n")
+
+    ("atom_only",
+    PO::value<bool>(&atom_only)->default_value(true),
+    "Triangulate only ATOM records.\n"
+    "Default: true.\n")
+
+    ("list_wall",
+    PO::value<std::string>(&list_wall)->default_value("none"),
     "atom: output a file with the wall atoms.\n"
     "residue: output a file with the wall amino acids.\n" 
     "none: don't.\n"
@@ -136,75 +282,11 @@ int get_parameters(int ac, char *av[], ANA::InOutOptions &io_opts,
     ("separator", PO::value<std::string>(&list_wall_separator)->default_value("\t"),
     "Separator character for wall residues (atoms). \"+\": might useful for pymol.\n"
     "Default: \t.\n")
-
-    ("ASA_discard_method", PO::value<std::string>(&ASA_method)
-    ->default_value("cm"), " none: don't.\n"
-    "cm: determine cells surrondings using the global CM as reference.\n"
-    "backbone: draw a convex hull between the Calphas and discard every cell with its centroid outside(inside) the hull.\n"
-    "axes: determine cells surrondings using its centroid and cartesian axes as references.\n"
-    "Default: cm.\n")
-
-    ("ASA_only_side", PO::value<std::string>(&only_side_ASA)
-    ->default_value("inside"), "inside: keep inside nulls.\n"
-    "outside: keep outside nulls.\n" 
-    "Default: inside.\n")
-
-    ("ASA_exclude_amino_acids", PO::value<std::string>(&exclude_ca_for_ASA)
-    ->default_value("none"), "Residues to exclude from the ASA algorithms.\n")
-
-    ("ASA_min_dot_pdt", PO::value<double>(&max_probe)->default_value(0.7)
-    ->composing(), "Minimum dot product to keep(discard) cell. As it increases "
-    "more cells are classified as being \"outside\". Default: 0.7.\n")
-
-    ("ASA_max_dist", PO::value<double>(&max_probe_length)->default_value(15)
-    ->composing(), "Maximum distance between cell and Calpha used to keep cell. The bigger this number is the better (but slower) the process gets.\n"
-    "Default: 15.\n")
-
-    ("start", PO::value<int>(&md_start)->default_value(1),
-    "Frame to begin reading at.\n"
-    "Default: 1.\n")
-
-    ("step", PO::value<int>(&md_step)->default_value(1),
-    "Step count to read trajectory frames. NetCDF format only.\n"
-    "Default: 1.\n")
-
-    ("stop", PO::value<int>(&md_end)->default_value(0),
-    "Frame to stop reading. Reads the whole trajectory when set to 0.\n"
-    "Default: 0.\n")
-
-    ("NDD_step", PO::value<int>(&NDD_opts._step)->default_value(3),
-    "1: ANA will not perform the derivative and instead output 2 files with the volumes of the displaced cavity in the positive and the negative direction.\n"
-    "2: ANA will output the VGV.\n"
-    "3: ANA will output the flexibility index.\n"
-    "Default: 3\n")
-
-    ("NDD_modes_format", PO::value<std::string>(&NDD_opts._modes_format)->default_value("row"),
-    "amber: vectors will be read as Amber PCA modes.\n"
-    "row(column): vectors will be read in row(column) major order.\n"
-    "Default: row.\n")
-
-    ("NDD_frequences_scaling", PO::value<bool>(&NDD_opts._scale_w_freqs)->default_value(false),
-    "If true ANA will scale the input vectors by their frequencies instead of automatically generating its own scaling factors.\n"
-    "Default: false.\n")
-
-    ("min_vol_radius", PO::value<double>(&cell_opts._minVR)->default_value(1.4)
-    ->composing(), "Radius of the sphere with the minimum volume to be taken into account.\n"
-    "Default: 1.4.\n")
-
-    ("max_area_radius", PO::value<double>(&cell_opts._maxSR)->default_value(99)
-    ->composing(), "Radius of the sphere with the maximum surface to be taken into account.\n"
-    "Default: 99.\n")
-
-    ("sphere_count", PO::value<int>(&sphere_count)->default_value(5)
-    ->composing(), "Sphere count for grid output.\n"
-    "Default: 5.\n")
-
-    ("output_type", PO::value<std::string>(&io_opts._out_type)
-    ->default_value("grid_pdb")->composing(), "raw_pdb: null areas as tetrahedrons (residues).\n"
-    "raw_cgo: null areas as tetrahedrons formed with CGO lines in a pymol script.\n"
-    "grid_pdb: null areas filled with points.\n"
-    "grid_cgo: null areas filled with CGO spheres in a pymol script.\n"
-    "Default: grid_pdb.\n");
+    
+    ("triangulate_only_included_aas",
+    PO::value<bool>(&triangulate_only_included_aas)->default_value(false),
+    "Instead of triangulating the whole molecule triangulate only the included amino acids.\n"
+    "Default: false.\n");
 
     // Now, map the variables
     PO::variables_map vm;
