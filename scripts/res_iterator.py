c1660210149f36f8188be63bd2974c7bfa194bab
pgbarletta <pbarletta@gmail.com>
2020-06-22 22:24:56 -0300

    renamed scripts

--- scripts/res_iterator.py
+++ scripts/res_iterator.py
@@ -1,4 +1,4 @@
 from pymol import cmd,stored
 stored.list = []
-cmd.iterate("(wacho and name ca)", "stored.list.append(resi)")
+cmd.iterate("(sele and name ca)", "stored.list.append(resi)")
 print stored.list
