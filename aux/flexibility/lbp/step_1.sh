fb47503ad57c161943621416d73e1b5ab02112dc
pgbarletta <pbarletta@gmail.com>
2020-07-13 19:36:23 -0300

    Done with all ANA tutorials

--- aux/flexibility/lbp/step_1.sh
+++ aux/flexibility/lbp/step_1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo -e "Alpha without ligand"
-ANA2 avg_4uet.pdb -c config_alfa.cfg -M modes_4uet -Z 5
-
-echo -e "\nAlpha with ligand"
-ANA2 avg_4xcp.pdb -c config_alfa.cfg -M modes_4xcp -Z 5
-
-echo -e "\nBeta without ligand"
+echo -e "Beta without ligand"
 ANA2 avg_1ifb.pdb -c config_beta.cfg -M modes_1ifb -Z 5
 
 echo -e "\nBeta with ligand"
 ANA2 avg_2ifb.pdb -c config_beta.cfg -M modes_2ifb -Z 5
+
+echo -e "\nAlpha without ligand"
+ANA2 avg_4uet.pdb -c config_alfa.cfg -M modes_4uet -Z 5
+
+echo -e "\nAlpha with ligand"
+ANA2 avg_4xcp.pdb -c config_alfa.cfg -M modes_4xcp -Z 5
 
 exit 0
