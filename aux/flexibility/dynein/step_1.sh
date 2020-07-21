72817aede9f92a9c9d1984c344b9587f251c59aa
pgbarletta <pbarletta@gmail.com>
2020-07-21 16:46:02 -0300

    Done fixing tutorials

--- aux/flexibility/dynein/step_1.sh
+++ aux/flexibility/dynein/step_1.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo -e "LC8 cavity C"
-ANA2 lc8.pdb -c ecf.cfg -M modes_lc8 -F frequencies_lc8
+ANA2 lc8.pdb -c ecf.cfg -M modes_lc8 -F frequencies_lc8 -Z 5
 
 echo -e "\nLC8 cavity D"
-ANA2 lc8.pdb -c edf.cfg -M modes_lc8 -F frequencies_lc8
+ANA2 lc8.pdb -c edf.cfg -M modes_lc8 -F frequencies_lc8 -Z 5
 
 echo -e "\nTcTex cavity C"
-ANA2 tctex.pdb -c acb.cfg -M modes_tctex -F frequencies_tctex
+ANA2 tctex.pdb -c acb.cfg -M modes_tctex -F frequencies_tctex -Z 5
 
 echo -e "\nTcTex cavity D"
-ANA2 tctex.pdb -c adb.cfg -M modes_tctex -F frequencies_tctex
+ANA2 tctex.pdb -c adb.cfg -M modes_tctex -F frequencies_tctex -Z 5
 
 exit 0
